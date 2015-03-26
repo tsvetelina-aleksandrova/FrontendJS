e6f3eccf4a254acbf478c927cfb21fb4cfa3d36b
tsvetelina-aleksandrova <tsvetelina-aleksandrova@tenkod.com>
2015-03-26 22:20:27 +0200

    week1-hw1

--- Week1/HW1/server.js
+++ Week1/HW1/server.js
@@ -2,15 +2,23 @@ var express = require('express')
 var app = express()
 app.use(express.static('public'));
 
-var vars = require('./vars.json')
+var indexVars = require('./index-data.json')
+var cartVars = require('./cart-data.json')
+var itemInfoVars = require('./item-info-data.json')
 
 app.set('views', './views')
-// need this if we want to skip explicitly writing .jade file extensions
 app.set('view engine', 'jade')
 
 app.get('/', function (req, res) {
-	//so now we can have file name "index" instead of "index.jade"
-  res.render('index', vars);
+  res.render('index', indexVars);
+})
+
+app.get('/cart', function (req, res) {
+  res.render('cart', cartVars);
+})
+
+app.get('/item-info', function (req, res) {
+  res.render('item-info', itemInfoVars);
 })
 
 var server = app.listen(3000, function () {
