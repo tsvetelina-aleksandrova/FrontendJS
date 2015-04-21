0e6b1d15392782d9713e6d812523ee151d07790e
tsvetelina-aleksandrova <tsvetelina-aleksandrova@tenkod.com>
2015-04-21 15:06:03 +0300

    add week6 task1

--- Week5/2-todo-crud/js/app.js
+++ Week5/2-todo-crud/js/app.js
@@ -1,3 +1,11 @@
+/*
+* add order to tasks
+* prioritize
+* and stuff
+* drag n drop them
+* and stuff
+*/
+
 var TaskList = (function() {
 	var tasks = [];
 	var idCounter = 1;
@@ -5,12 +13,12 @@ var TaskList = (function() {
 	var createTask = function(newTaskName) {
 		tasks.push({"id": idCounter, 
 					"name": newTaskName,
-					"finished": false
+					"finished": false,
 				});
 		idCounter += 1;
 
 		console.log("Added task with name " + newTaskName);
-		console.log("Current tasks are: " + tasksAsString());
+		console.log(tasksAsString());
 	}
 
 	var finishTask = function(id) {
@@ -22,11 +30,17 @@ var TaskList = (function() {
 		});
 
 		console.log("Finished task with id " + id);
-		console.log("Current tasks are: " + tasksAsString());
+		console.log(tasksAsString());
 	}
 
 	var displayList = function($container) {
 		$container.empty();
+
+		$container[0].addEventListener("drop", drop(event));
+		$container[0].addEventListener("dragover", allowDrop(event));
+			//.on("drop", drop(event))
+			//.on("dragover", allowDrop(event));
+
 		tasks.forEach(function(task) {
 			var $taskElem = createTaskDomElem($container, task);
 			$container.append($taskElem);
@@ -50,23 +64,69 @@ var TaskList = (function() {
 		var $newTaskLabel = $("<label></label>")
 							 .html(taskName)
 							 .addClass(["", "finished-task"][+task.finished]);
+		var $newTaskDeleteBtn = $("<button></button>")
+								.addClass("delete-task-btn")
+								.html("Delete")
+								.on("click", function() {
+									deleteTask($cont, taskId);
+								});
 
 		var $newTask = $("<li></li>")
 						.append($newTaskBox)
-						.append($newTaskLabel);
+						.append($newTaskLabel)
+						.append($newTaskDeleteBtn)
+						.attr("draggable", "true");
+		$newTask[0].addEventListener("dragstart", drag(event));
 
 		return $newTask;
 	}
 
+	var deleteTask = function($container, taskId) {
+		tasks = tasks.filter(function(task) {
+			return task.id != taskId;
+		});
+
+		displayList($container);
+
+		console.log("Deleted task with id " + taskId);
+		console.log(tasksAsString());
+	}
+
+	function allowDrop(evt) {
+    //drag over
+evt.preventDefault();
+//specify operation
+evt.dataTransfer.dropEffect = "move";
+}
+
+function drag(evt) {
+   //start drag
+source=evt.target;
+//set data
+evt.dataTransfer.setData("text/plain", evt.target.innerHTML);
+//specify allowed transfer
+evt.dataTransfer.effectAllowed = "move";
+}
+
+function drop(evt) {
+    //drop
+evt.preventDefault();
+evt.stopPropagation();
+//update text in dragged item
+//update text in drop target
+//evt.target.innerHTML = evt.dataTransfer.getData("text/plain");
+}
+
 	var tasksAsString = function() {
 		if(tasks.length === 0) {
-			return "\nNo tasks are available\n";
+			return "No tasks are currently available";
 		}
-		return tasks.reduce(function(result, item) {
-			return result + ["\n", "ID: ", item.id, 
+		return "Current tasks are: " + 
+			tasks.reduce(function(result, item) {
+				return result + ["\n", "ID: ", item.id, 
 							"; Name: ", item.name,
 							"; Finished: ", item.finished].join("");
-		}, "");
+			}, "");
 	}
 
 	return {
