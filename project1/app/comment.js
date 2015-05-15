04e6316a0f38acb3efeeb828b20a5c5f5bf088b1
tsvetelina-aleksandrova <tsvetelina-aleksandrova@tenkod.com>
2015-05-16 02:19:06 +0300

    delete unused files

--- project1/app/comment.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
- 
-module.exports = mongoose.model("Comments", {
-    pieceId: String,
-    writer: String,
-    text: String
-}, "comments");
--- project1/app/db/user.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
- 
-module.exports = mongoose.model("Users", {
-    username: String,
-    password: String,
-    img: String,
-    email: String
-}, "users");
