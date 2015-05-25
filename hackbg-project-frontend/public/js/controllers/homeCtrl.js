4a03063577a4010756e730600b7a2f2293b37765
tsvetelina-aleksandrova <tsvetelina-aleksandrova@tenkod.com>
2015-05-25 03:13:07 +0300

    example top items list

--- hackbg-project-frontend/public/js/controllers/homeCtrl.js
+++ hackbg-project-frontend/public/js/controllers/homeCtrl.js
@@ -2,17 +2,25 @@ var HomeCtrl = (function(){
 
 	var init = function(){
 		helpers.displayWithJade($("header"), "/views/user-header-nav.jade");
-		helpers.displayWithJade($(".content"), "/views/homejade");
-		/*.then(function(){
+		helpers.displayWithJade($(".content"), "/views/home.jade")
+		.then(function(){
+			$('header').hover(function(event){
+				$("#main-nav").removeClass("hidden");
+				$("#main-nav").addClass("visible");
+				event.preventDefault();
+			}, function(event){
+				$("#main-nav").addClass("hidden");
+				$("#main-nav").removeClass("visible");
+			});
 
 			$('#fullpage').fullpage({
-		      anchors:['loginPage', 'aboutPage', 'signupPage'],
+		      anchors:['newest', 'top', 'categories'],
 		      navigation: true,
 		      navigationPosition: 'left',
-		      navigationTooltips: ['Log in', 'About', 'Sign up'],
+		      navigationTooltips: ['Newest', 'Top rated', 'Categories'],
 		      menu: '#header-nav-menu'
 		    });
-		});*/
+		});
 	}
 
 	return {
